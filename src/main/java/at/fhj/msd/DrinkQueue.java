3bdd8750bd24c4e675f92bad3dddd5381af9148d
FrontendTF <t.feyerl@gmail.com>
2025-04-25 18:04:54 +0200

    Add offer, poll, remove, peek methods to DrinkQueue (Thomas part)

--- src/main/java/at/fhj/msd/DrinkQueue.java
+++ src/main/java/at/fhj/msd/DrinkQueue.java
@@ -1,8 +1,35 @@
 package at.fhj.msd;
 
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+import java.util.Queue;
+
 /**
  * A queue that stores Drink objects.
  */
 public class DrinkQueue {
-   
+    private Queue<Drink> drinks = new LinkedList<>();
+
+    public boolean offer(Drink d) {
+        return drinks.offer(d);
+    }
+
+    public Drink poll() {
+        return drinks.poll();
+    }
+
+    public Drink remove() {
+        Drink d = drinks.poll();
+        if (d == null) throw new NoSuchElementException("No drink in queue");
+        return d;
+    }
+
+    public Drink peek() {
+        return drinks.peek();
+
+    // Russman Teil    
+
+
+
+    }
 }
